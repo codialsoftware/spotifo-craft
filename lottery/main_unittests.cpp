3afba951cfec0a0f1376b04c8f186f56dd05d0bf
leszek.porebski@gmail.com <leszek.porebski@gmail.com@58557f06-7ed8-efe1-588b-e96032ed85c2>
2012-11-04 00:50:59 +0000

    [lottery] move default main implementation to shared folder

--- lottery/main_unittests.cpp
+++ lottery/main_unittests.cpp
@@ -1,3 +1,4 @@
+#if defined(UNITTEST)
 #include "shared/unittest.h"
 #include "Lottery.h"
 #include <string>
@@ -28,3 +29,4 @@ namespace {
 int run_unittest() {
     return UnitTest::RunAllTests();
 }
+#endif
