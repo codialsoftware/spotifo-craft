da693e695b51d468b1ffe49460f957c080e1f78b
leszek.porebski@gmail.com <leszek.porebski@gmail.com@58557f06-7ed8-efe1-588b-e96032ed85c2>
2012-11-07 04:17:18 +0000

    [lottery] handling input stream. reading data from cin

--- trunk/lottery/Lottery.hpp
+++ trunk/lottery/Lottery.hpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <cmath>
 #include <algorithm>
+#include <iostream>
 #include <vector>
 
 namespace SpotifyPuzzles { namespace Lottery {
@@ -16,6 +17,11 @@ namespace SpotifyPuzzles { namespace Lottery {
         Algorithm m_tmpAlg;
         Algorithm& m_Algorithm;
 
+        int m_Friends;
+        int m_Attendees;
+        int m_Drawns;
+        int m_Tickets;
+
         public:
             Calculator()
                 : m_tmpAlg(Algorithm())
@@ -26,6 +32,20 @@ namespace SpotifyPuzzles { namespace Lottery {
 
             double operator()(int attendees, int drawn, int tickets, int friends) {
                 return m_Algorithm(attendees, drawn, tickets, friends);
+            }
+
+            operator std::string() const {
+                return impl::convert(m_Algorithm(m_Attendees, m_Drawns, m_Tickets, m_Friends));
+            }
+
+            friend std::ostream& operator<< (std::ostream& out, const Calculator& mThis) {
+                 out << (std::string)mThis;
+                 return out;
+            }
+
+            friend std::istream& operator>> (std::istream& in, Calculator& mThis) {
+                in >> mThis.m_Attendees >> mThis.m_Drawns >> mThis.m_Tickets >> mThis.m_Friends;
+                return in;
             }
     };
 }}
--- trunk/lottery/main_default.cpp
+++ trunk/lottery/main_default.cpp
@@ -1,5 +1,14 @@
+#include <iostream>
 #include "shared/main.h"
+#include "algorithms/MultiplicatoryAlgorithm.hpp"
+#include "Lottery.hpp"
 
+using namespace std;
+using namespace SpotifyPuzzles::Lottery;
 int run() {
+    Calculator<Algorithms::MultiplicatoryAlgorithm> calculator;
+
+    cin >> calculator;
+    cout << calculator << endl;
     return 0;
 }
